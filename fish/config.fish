5b292cbca5fdf177cda9e00fc741176049290e50
Nóra Szepes <nora.szepes@tenforce.com>
2016-10-10 14:35:56 +0200

    updated atom packages, background, fish commands

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ white)'@'(set_color yellow)(hostname)' '
     test $USER = 'root'; and echo (set_color red)"#"
 
     # Main
-	echo -n (set_color 00FFFF)\[(set_color FFFF00)$USER: (set_color FF0000)(prompt_pwd)(set_color 00FFFF)\]\$' '
+	echo -n (set_color 00FF00)\[(set_color FFFF00)$USER: (set_color FF0000)(prompt_pwd)(set_color 00FF00)\]\$' '
 end
 
 
@@ -22,11 +22,16 @@ alias gs='git status'
 alias ga='git add'
 alias ga.='git add .'
 alias gcm='git commit'
+alias gcma='git commit -a'
 alias gd='git diff'
 alias gpl='git pull'
 alias gps='git push'
 alias grb='git rebase'
 alias gch='git checkout'
+alias gstl='git stash list'
+alias gst='git stash'
+alias gsta='git stash apply'
+alias gstd='git stash drop'
 
 # ember
 alias e='ember'
